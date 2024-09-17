5b1052c3f99b50e313fd17f1fb800311f7320e03
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-17 14:38:51 +0200

    created constructor

--- src/net/htlgkr/GumpelmeierA21044/pos3/hue1/ErasthotenesPrimeSieve.java
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue1/ErasthotenesPrimeSieve.java
@@ -1,9 +1,20 @@
 package net.htlgkr.GumpelmeierA21044.pos3.hue1;
 
 public class ErasthotenesPrimeSieve implements PrimeSieve{
+    private int limit;
+
+    public ErasthotenesPrimeSieve(int limit) {
+        this.limit = limit;
+    }
+
     @Override
     public boolean isPrime(int p) {
-        return false;
+       for(int i = 2; i < p; i++) {
+           if(p % i == 0) {
+               return false;
+           }
+       }
+       return true;
     }
 
     @Override
