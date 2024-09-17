5e96bcf5df8732a95927ca90f3b0e2b14dec9f96
Alessandro Gumpelmeier <GumpelmeierA21044@sus.htl-grieskirchen.at>
2024-09-17 14:35:47 +0200

    created class ErasthotenesPrimeSieve and implemented interface

---
+++ src/net/htlgkr/GumpelmeierA21044/pos3/hue1/ErasthotenesPrimeSieve.java
@@ -0,0 +1,13 @@
+package net.htlgkr.GumpelmeierA21044.pos3.hue1;
+
+public class ErasthotenesPrimeSieve implements PrimeSieve{
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
